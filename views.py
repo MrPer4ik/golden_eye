373f44fbb250dbeb5811ed2f6929ca0cc8ba3fa8
Alexander Khomych <arkhomych.lit@gmail.com>
2020-03-27 21:52:58 +0200

    Add manual update for rates

--- views.py
+++ views.py
@@ -1,6 +1,21 @@
+from functools import wraps
+
+from flask import request, abort
 
 from app import app
 import controllers
+from config import IP_LIST
+
+
+def check_ip(func):
+    @wraps(func)
+    def checker(*args, **kwargs):
+        if request.remote_addr not in IP_LIST:
+            return abort(403)
+
+        return func(*args, **kwargs)
+
+    return checker
 
 
 @app.route("/")
@@ -25,5 +40,12 @@ def update_xrates(from_currency=None, to_currency=None):
 
 
 @app.route("/logs")
+@check_ip
 def view_logs():
     return controllers.ViewLogs().call()
+
+
+@app.route("/edit/<int:from_currency>/<int:to_currency>", methods=["GET", "POST"])
+@check_ip
+def edit_xrate(from_currency, to_currency):
+    return controllers.EditRate().call(from_currency, to_currency)
