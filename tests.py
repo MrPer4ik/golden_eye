cfe6df451197d1f6edf51b72a05633c17b123d55
Alexander Khomych <arkhomych.lit@gmail.com>
2020-03-27 10:07:04 +0200

    Add API methods

--- tests.py
+++ tests.py
@@ -3,6 +3,7 @@ import json
 from unittest.mock import patch
 
 import requests
+import xmltodict
 
 import models
 import api
@@ -138,6 +139,7 @@ class Test(unittest.TestCase):
 
         api.HTTP_TIMEOUT = 15
 
+    @unittest.skip('skip')
     def test_cryptonator_uah(self):
         from_currency = 1000
         to_currency = 980
@@ -160,6 +162,31 @@ class Test(unittest.TestCase):
         self.assertIsNotNone(api_log.response_text)
 
         self.assertIn('{"base":"BTC","target":"UAH","price":', api_log.response_text)
+
+    def test_xml_api(self):
+        r = requests.get("http://localhost:5000/api/xrates/xml")
+        self.assertIn("<xrates>", r.text)
+        print(r.text)
+        xml_rates = xmltodict.parse(r.text)
+        self.assertIn("xrates", xml_rates)
+        self.assertIsInstance(xml_rates["xrates"]["xrate"], list)
+        self.assertEqual(len(xml_rates["xrates"]["xrate"]), 5)
+
+    def test_json_api(self):
+        r = requests.get("http://localhost:5000/api/xrates/json")
+        json_rates = r.json()
+        self.assertIsInstance(json_rates, list)
+        self.assertEqual(len(json_rates), 5)
+        for rate in json_rates:
+            self.assertIn("from", rate)
+            self.assertIn("to", rate)
+            self.assertIn("rate", rate)
+
+    def test_json_api_uah(self):
+        r = requests.get("http://localhost:5000/api/xrates/json?to_currency=980")
+        json_rates = r.json()
+        self.assertIsInstance(json_rates, list)
+        self.assertEqual(len(json_rates), 2)
 
 
 if __name__ == '__main__':
--- views.py
+++ views.py
@@ -1,3 +1,4 @@
+
 from app import app
 import controllers
 
@@ -9,4 +10,9 @@ def hello():
 
 @app.route("/xrates")
 def view_rates():
-    return controllers.get_all_rates()
+    return controllers.ViewAllRates().call()
+
+
+@app.route("/api/xrates/<fmt>")
+def api_rates(fmt):
+    return controllers.GetApiRates().call(fmt)
