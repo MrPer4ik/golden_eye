061318a11d5c8e3dd1f2b48fcffb83df86a5e11d
Alexander Khomych <arkhomych.lit@gmail.com>
2020-03-28 16:50:28 +0200

    Add Heroku settings

--- api/__init__.py
+++ api/__init__.py
@@ -54,4 +54,11 @@ class _Api:
             log.save()
 
     def _send(self, url, method, data=None, headers=None):
+        headers = {"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:66.0) Gecko/20100101 Firefox/66.0",
+                   "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+                   "Accept-Language": "en-US,en;q=0.5",
+                   "Accept-Encoding": "gzip, deflate",
+                   "DNT": "1",
+                   "Connection": "close",
+                   "Upgrade-Insecure-Requests": "1"}
         return requests.request(method=method, url=url, headers=headers, data=data, timeout=HTTP_TIMEOUT)
